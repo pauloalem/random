06da6026820029442dea3239ffad7b7e2271c0df
Paulo Alem <paulo.alem@investtools.com.br>
2011-01-27 17:31:51 -0200

    Corrigindo para usar a funcao de geracao de numero aleatoria correta

--- montecarlopi.py
+++ montecarlopi.py
@@ -11,21 +11,20 @@ http://www.eveandersson.com/pi/monte-carlo-circle
 def calcPi(rng):
     hits = 0
     for i in xrange (0, 1000000):
-        x = random.random()
-        y = random.random()
+        x = rng.random()
+        y = rng.random()
         dist = sqrt(pow(x, 2)+pow(y, 2))
         if dist <= 1.0:
             hits = hits + 1.0
 
     # hits / i = 1/4 Pi
     pi = 4 * (hits / i)
+    return pi
 
-    print "pi = %s" %(pi)
 
-
-calcPi(LinearCongruential())
-calcPi(ParkMiller())
-calcPi(MersenneTwister())
+print calcPi(LinearCongruential())
+print calcPi(ParkMiller())
+print calcPi(MersenneTwister())
 
 #Usando o modulo random da biblioteca padrao do python, que nada mais é do que um mersenne twister em C
-calcPi(random.random)
+print calcPi(random)
